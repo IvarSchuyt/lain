2c0479dc0a4bbdc320970de580f5bc0d4d43c323
Ivar Schuyt <112855849+IvarSchuyt@users.noreply.github.com>
2024-11-27 17:11:24 +0100

    yes

---
+++ src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<main>
+
+<svg>
+
+<h1>Welcome</h1>
+<p>
+    Project Artichoke was a mind control program that gathered information together with the intelligence divisions of the Army, Navy, Air Force and FBI. In addition, the scope of the project was outlined in a memo dated January 1952 that asked, "Can we get control of an individual to the point where he will do our bidding against his will and even against fundamental laws of nature, such as self-preservation?"
+    <br><br>
+    Project Artichoke was the Central Intelligence Agency's secret code name for carrying out in-house and overseas experiments using LSD, hypnosis and total isolation as forms of physiological harassment for special interrogations on human subjects. At first agents used cocaine, marijuana, heroin, peyote and mescaline, but they increasingly saw LSD as the most promising drug. The subjects who left this project were fogged with amnesia, resulting in faulty and vague memories of the experience. In 1952, unknowing CIA agents were secretly drugged to determine the drug's effects on unsuspecting people. One record states that an agent was kept on LSD for 77 days.
+    <br><br>
+    Artichoke researched the potential of dengue fever and other diseases. A declassified Artichoke memo read: "Not all viruses have to be lethal… the objective includes those that act as short-term and long-term incapacitating agents."
+    <br><br>
+    The CIA disputed which department would take over the operation. Finally, it was decided that an agent from the CIA research staff, former U.S. Army brigadier general Paul F. Gaynor, would oversee it. The CIA sought to establish control over what it perceived as the "weaker" and "less intelligent" segments of society, or for potential agents, defectors, refugees, prisoners of war and others. A CIA report states that if hypnosis succeeded, assassins could be created to assassinate "a prominent [redacted] politician or if necessary, [an] American official." The overseas operations took place in locations throughout Europe, Japan, Southeast Asia and the Philippines. Teams were assembled to manage these operations and they were told to "conduct at the overseas bases operational experiments utilizing an alien as a subject."
+</p>
+
+</svg>
+
+</main>
+
+
+<style lang="scss">
+
+    main{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        padding: 0 20%;
+    }
+
+</style>
